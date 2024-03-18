f2f602c816c6a1ec46dceea39bf10cbaa3249df2
spark <98943801+Sonar135@users.noreply.github.com>
2024-03-18 12:28:09 +0100

    Add files via upload

---
+++ header.php
@@ -0,0 +1,131 @@
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <script src="https://kit.fontawesome.com/b782cf5553.js" crossorigin="anonymous"></script>  
+    <link rel="icon" type="image/x-icon" href="images\kisspng-babcock-university-university-of-ibadan-academic-d-5b1c90eb26da71.7889147215285987631592-removebg-preview.png">
+    <link rel="stylesheet" href="css/header.css?v=<?php echo time();?>">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Medique</title>
+</head>
+<body>
+
+<div class="loader">
+  <img src="images\8-removebg-preview.png" alt="">
+</div>
+    <div class="nav">
+
+        <div class="nav_cont">
+            <div class="logo_cont">
+          <a href="main.php"> <h1>MEDIQUE</h1></a>  
+            </div>
+
+            <div class="menu">
+                <ul>
+
+                <li>
+                    <a href="main.php">  home</a>
+                  </li>
+
+          
+                  
+                  
+                    
+                  <?php
+            if(isset($_SESSION["id"])){
+
+
+                if($user_type=='planner'){
+                  echo '  <li>
+                  <a href="plan.php">Plan Event</a>
+                </li>
+
+                <li id="chat_menu">
+                <div class="tot '.$tot_none.'" >'.$total_num.'</div>
+                <a href="chat.php">chat</a>
+              </li>
+              
+              ';
+                }
+
+             
+
+             echo '   <li>
+             <a href="calendar_oop.php">calendar</a>
+           </li>
+           
+
+         
+           ';
+
+           if($user_type=='planner'){
+            echo '  <li>
+            <a href="contact_admin.php"> Contact</a>
+           </li>';
+          }
+            }
+
+
+         
+            ?>
+
+                    
+
+
+                      
+
+
+                   
+
+
+                  
+                    
+               
+                 
+
+                    <?php
+            if(isset($_SESSION["id"])){
+                if($user_type=='admin'){
+                  echo '    <li>
+                  <a href="admin.php">admin</a>
+                 </li>';
+                }
+                echo '  <a href="logout.php">Logout</a>';
+
+            }
+
+            else{
+              echo '     <a href="reg.php">register/login</a>';
+            }
+            ?>
+
+   
+                      
+
+          
+                </ul>
+            </div>
+        </div>
+        <span id="line"></span>
+    </div>
+</body>
+<script src="js/min.js"></script>
+<script src="js/dom.js"></script>
+
+<script>
+  $(window).on("load", ()=>{
+    $(".loader").fadeOut("slow")
+    $("body").css("overflow-y", "scroll");
+  });
+</script>
+</html>
+
+
+
+
+
+
---
+++ main.php
@@ -0,0 +1,54 @@
+<?php
+    include "header.php";
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="css/main.css?v=<?php echo time();?>">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div class="img_container">
+          <div class="overlay">
+            <div class="container">
+                <div class="cent">
+             <div class="hanger">
+
+             </div>
+                  <div class="some_text">
+                    <h4>We are here for you</h4>
+
+                    <h1>The best medical service in the country</h1>
+
+                    <h4></h4>
+                  </div>
+
+
+                  <div class="img">
+
+                  </div>
+                </div>
+            </div>
+            </div>
+
+    </div>
+
+
+    <div class="container sec1">
+      
+    </div>
+
+
+
+
+    <?php 
+        // include "footer.php";
+    ?>
+</body>
+</html>
